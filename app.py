21e107ce49377c971faa9a5654ae673549080716
Tech Kaya <techkayacodes@gmail.com>
2023-11-19 12:59:46 -0500

    finishing up modifying app.py and readme.md

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Change this to a random secret key
+app.secret_key = '595-359-861'  # Change this to a random secret key
 
 def get_db_connection():
     conn = sqlite3.connect('hw13.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+@app.route('/')
+def home():
+    # Redirect to login page or another appropriate page as the homepage
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,7 +37,53 @@ def dashboard():
 
     return render_template('dashboard.html', students=students, quizzes=quizzes)
 
-# Add routes for adding students, quizzes, viewing results, etc.
+@app.route('/student/add', methods=['GET', 'POST'])
+def add_student():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO students (first_name, last_name) VALUES (?, ?)', (first_name, last_name))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_student.html')
+
+@app.route('/quiz/add', methods=['GET', 'POST'])
+def add_quiz():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        subject = request.form['subject']
+        question_count = request.form['question_count']
+        quiz_date = request.form['quiz_date']
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO quizzes (subject, question_count, quiz_date) VALUES (?, ?, ?)', (subject, question_count, quiz_date))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_quiz.html')
+
+@app.route('/student/<int:student_id>')
+def student_results(student_id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    conn = get_db_connection()
+    results = conn.execute('SELECT * FROM results WHERE student_id = ?', (student_id,)).fetchall()
+    conn.close()
+
+    return render_template('student_results.html', results=results, student_id=student_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
