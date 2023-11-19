0575658f7dc2c26e54bf85ece38fd4e26a036167
Tech Kaya <techkayacodes@gmail.com>
2023-11-19 12:42:23 -0500

    modified all html files, added styles, and app.py

--- initialize-app.py
+++ initialize-app.py
@@ -1,20 +1,19 @@
 import sqlite3
 
-def initialize_database():
+def initialize_db():
     conn = sqlite3.connect('hw13.db')
-    cursor = conn.cursor()
+    with open('schema.sql') as f:
+        conn.executescript(f.read())
 
-    # Create tables
-    with open('schema.sql', 'r') as f:
-        cursor.executescript(f.read())
+    cur = conn.cursor()
 
-    # Insert initial data
-    cursor.execute("INSERT INTO students (first_name, last_name) VALUES (?, ?)", ("John", "Smith"))
-    cursor.execute("INSERT INTO quizzes (subject, question_count, quiz_date) VALUES (?, ?, ?)", ("Python Basics", 5, "2015-02-05"))
-    cursor.execute("INSERT INTO results (student_id, quiz_id, score) VALUES (?, ?, ?)", (1, 1, 85))
+    # Insert sample data
+    cur.execute("INSERT INTO Students (first_name, last_name) VALUES (?, ?)", ('John', 'Smith'))
+    cur.execute("INSERT INTO Quizzes (subject, question_count, quiz_date) VALUES (?, ?, ?)", ('Python Basics', 5, '2015-02-05'))
+    cur.execute("INSERT INTO Results (student_id, quiz_id, score) VALUES (?, ?, ?)", (1, 1, 85))
 
     conn.commit()
     conn.close()
 
-if __name__ == "__main__":
-    initialize_database()
+if __name__ == '__main__':
+    initialize_db()
--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
-CREATE TABLE students (
+CREATE TABLE Students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
 
-CREATE TABLE quizzes (
+CREATE TABLE Quizzes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     subject TEXT NOT NULL,
     question_count INTEGER NOT NULL,
-    quiz_date TEXT NOT NULL
+    quiz_date DATE NOT NULL
 );
 
-CREATE TABLE results (
+CREATE TABLE Results (
     student_id INTEGER,
     quiz_id INTEGER,
     score INTEGER,
-    FOREIGN KEY(student_id) REFERENCES students(id),
-    FOREIGN KEY(quiz_id) REFERENCES quizzes(id)
+    FOREIGN KEY (student_id) REFERENCES Students(id),
+    FOREIGN KEY (quiz_id) REFERENCES Quizzes(id)
 );
